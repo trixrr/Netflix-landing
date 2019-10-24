e51e8de20ae86c3b4849ae8ff844d4e49bc2edf7
Lee Lashbrook <leelashbrook@Lees-MacBook-Pro-2.local>
2019-10-24 17:32:56 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+// select tab content items
+function selectItem(e) {
+  removeBorder();
+  removeShow();
+  // add border to current tab
+  this.classList.add('tab-border');
+
+  // grab content item from DOM
+  const tabContenItem = document.querySelector(`#${this.id}-content`);
+  // add show class
+  tabContenItem.classList.add('show');
+}
+
+function remove() {
+  tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+
+function removeShow() {
+  tabItems.forEach(item => item.classList.remove('show'));
+}
+
+
+
+// listen for tab click
+tabItems.forEach(item => item.addEventListener('click', selectItem));
